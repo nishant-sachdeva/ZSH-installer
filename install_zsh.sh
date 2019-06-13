45cbcbf84e96d788af762a69d78d96100a5b076c
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2019-06-13 20:01:03 +0530

    script uploaded

---
+++ install_zsh.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+#this script is a series of steps for installing and setting up zsh on your pc
+
+# ubuntu 18.04
+
+
+sudo apt install zsh
+sudo apt-get install powerline fonts-powerline
+
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+
+chsh -s /bin/zsh
