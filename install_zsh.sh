0724d8ad834353a0509467d6fcfa75a75719b570
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2020-05-05 08:32:44 -0700

    restart message added

--- install_zsh.sh
+++ install_zsh.sh
@@ -13,3 +13,5 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 chsh -s /bin/zsh
+
+echo "Now restart the System to see Zsh into action"
